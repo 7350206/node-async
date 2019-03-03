1fc4d5ad3934766b0b23a5db815ab4f30a129278
pl <7350206@pl.com>
2019-03-03 17:31:36 +0300

    a problem described

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 console.log('Before');
-setTimeout(() => {
-  console.log('reading db...');
-}, 1000)
+const user = getUser(1)
+console.log(user); //undefined for now
 console.log('After');
+
+// callbacks
+// promises-async/await
+
+function getUser(id){
+  setTimeout(() => {
+    console.log('reading db for user...');
+    return { id, user:"Connor" }
+  }, 1000) //doesnt block, pass itself through
+}
